2dfded2d9ff04e33430be51496f331f0a16dd08a
RohitRana8366 <rrohit8366@gmail.com>
2025-10-29 22:11:24 +0530

    Add average calculation script to compute the average of two floating-point numbers.

---
+++ average.py
@@ -0,0 +1,4 @@
+num1=float(input("enter your first floating number:"))
+num2=float(input("enter your second  floating number:"))
+average =num1+num2/2
+print ("your average of this two number=",average)
